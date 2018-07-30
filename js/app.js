aca716949748bde258d823978ba2489592da643c
mugala <mugalalevi21@gmail.com>
2018-07-30 15:04:31 +0300

    Filter function This commit will flter using the forEach method and return the results that matches the filter

--- js/app.js
+++ js/app.js
@@ -73,3 +73,16 @@ function clearTasks(){
 
     //https://jsperf.com/innerhtml-vs-removechild
 }
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+    
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    });
+} 
