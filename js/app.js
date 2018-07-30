c4d8e965785061acead569df3e160590fd0fb130
mugala <mugalalevi21@gmail.com>
2018-07-30 15:43:12 +0300

    DOM Load event This commit will create an event listener to listen after the document has loaded

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners(){
+    //DOM Load event
+    document.addEventListener('DOMContentLoaded', getTasks);
     // Add task event
     form.addEventListener('submit', addTask);
     // remove task event
