6f7c44c5163540856b9a487889ba1521c945b373
mugala <mugalalevi21@gmail.com>
2018-07-30 15:39:39 +0300

    Set localStorage for tasks This commit will create the storeTasksInLocalStorage function that persists the task in local storage

--- js/app.js
+++ js/app.js
@@ -47,10 +47,27 @@ function addTask(e){
 
     taskList.appendChild(li);
 
+    // Store in local storage
+    storeTaskInLocalStorage(taskInput.value);
+
     // Clear input
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Store task
+
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks')=== null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(tasks);
+
+    localStorage.setItem('tasks',JSON.stringify(task));
 }
 
 // Remove Task
